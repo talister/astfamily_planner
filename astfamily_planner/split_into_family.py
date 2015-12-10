0e5efbffc5357549ae8ba27299468afe5f662a06
Tim Lister <tlister@lcogt.net>
2015-12-09 17:21:13 -0800

    Add code to split asteroid family file into family members. Include master Milani et al. (2014) data file and generated file for (4) Vesta family

---
+++ astfamily_planner/split_into_family.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os
+
+family = 4 
+data_file = os.path.join('../', 'data','all.members')
+
+try:
+    data_fh = open(data_file, 'r')
+except:
+    os.exit()
+
+print "Reading", data_file,", looking for family #", family
+family_file =  os.path.join('../', 'data', 'family_' + str(family) + '.members')
+family_fh = open(family_file, 'w')
+print >> family_fh, "#ast.name  Hmag   status"
+
+num_family = 0
+
+for line in data_fh.readlines():
+    if line.lstrip()[0] == '%' or line.lstrip()[0] == "#" :
+        print "Skipped header"
+        continue
+    chunks = line.strip().split()
+    family1 = int(chunks[3])
+    if family1 == family:
+        asteroid = chunks[0]
+        H_mag = float(chunks[1])
+        status = int(chunks[2])
+        print "Found match for %9s with H=%.2f and status=%d" % (asteroid, H_mag, status)
+        print >> family_fh, "%-9s %.2f %d" % (asteroid, H_mag, status)
+        num_family += 1
+print "Wrote %d family members to %s" % ( num_family, family_file )
+
+data_fh.close()
+family_fh.close()
