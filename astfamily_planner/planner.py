7cafc5959dd381aa7764e8de746e9b8f052d7a34
Tim Lister <tlister@lcogt.net>
2015-12-15 18:08:33 -0800

    Add code to determine the range of phase angles, split it up into user specified no. of intervals and then compute windows, speed and exposure time for each of these windows and submit them to the scheduler.

--- astfamily_planner/planner.py
+++ astfamily_planner/planner.py
@@ -2,10 +2,13 @@
 import os
 import json
 from datetime import datetime, timedelta
+import numpy as np
 
 import logging
 
 from ephem_subs import call_compute_ephem, compute_ephem, determine_darkness_times, determine_exptime
+from source_subs import submit_block_to_scheduler
+from time_subs import datetime2mjd_utc
 
 def read_elements_list(data_path, family):
     
@@ -14,6 +17,47 @@ def read_elements_list(data_path, family):
         bodies = json.load(f)
 
     return bodies
+
+def determine_phaseangle_windows(obs_array, nsteps=10, dbg=True):
+
+    index_max_phangle =  np.argmax(obs_array[:,1])
+    if dbg: print "Max value of phaseangle = %4.1f at index %4d %s"  % ( np.amax(obs_array[:,1]), index_max_phangle, obs_array[index_max_phangle])
+    
+    index_min_phangle =  np.argmin(obs_array[:,1])
+    if dbg: print "Min value of phaseangle = %4.1f at index %4d %s"  % ( np.amin(obs_array[:,1]), index_min_phangle, obs_array[index_min_phangle])
+
+    phangle_range = obs_array[index_max_phangle][1] - obs_array[index_min_phangle][1]
+    phangle_step = phangle_range / float(nsteps)
+    if dbg: print "Range, steps of phaseangle= %.2f %.2f" % (phangle_range, phangle_step)
+
+    phang_step = 0
+    phang_windows = []
+    while phang_step <= nsteps:
+        phang = obs_array[index_min_phangle][1] + phang_step * phangle_step
+        phang_max = phang+(0.1*phangle_step)
+        phang_min = phang-(0.1*phangle_step)
+        if dbg: print "For phase angle %.1f->%.1f" % ( phang_min, phang_max)
+        subset = obs_array[np.where((obs_array[:,1] >= phang_min) & (obs_array[:,1] <= phang_max))]
+        if dbg: print subset
+        if len(subset) > 0:
+            phang_windows.append( [np.amin(subset[:,0]), np.amax(subset[:,0])] )
+        phang_step += 1
+
+    return phang_windows
+
+def determine_params(exp_length, site_code, start_time, end_time, group_id):
+    params = {'proposal_id': 'LCO2015B-016',
+              'user_id': 'tlister@lcogt.net',
+              'tag_id': 'LCOGT',
+              'priority': 15,
+              'exp_count': 2,
+              'exp_time': exp_length,
+              'site_code':  site_code,
+              'start_time': start_time,
+              'end_time': end_time,
+              'group_id': group_id
+              }
+    return params
 
 family = '4_trunc'
 data_path =  os.path.join('../', 'data')
@@ -25,6 +69,7 @@ alt_limit = 30.0
 mag_limit = 21.5
 pixel_scale = 0.389
 max_exptime = 240.0
+nsteps = 10
 
 bodies = read_elements_list(data_path, family)
 print "Read in ", len(bodies), "asteroids and elements."
@@ -34,16 +79,26 @@ logging.basicConfig(filename='planner.log', level=logging.INFO)
 line_fmt = "%s    %s %s    %s    %s   %3s   %3s  %4s %s  %5.1f"
 for asteroid in bodies:
     ast_name = str(asteroid.keys()[0])
-    ast_file = os.path.join(data_path, ast_name.replace(' ', '') + '_output')
-    ast_fh = open(ast_file, 'w')
-    num_written = 0
     for site in sites:
+        ast_file = os.path.join(data_path, ast_name.replace(' ', '') + '_' + site + '_output')
+        ast_fh = open(ast_file, 'w')
+        num_written = 0
         ast_elems = asteroid.values()[0]
+        ast_elems['current_name'] = ast_name
+# Compute epoch of the elements as a MJD
+        try:
+            epochofel = datetime.strptime(ast_elems['epochofel'], '%Y-%m-%d %H:%M:%S')
+        except TypeError:
+            epochofel = ast_elems['epochofel']
+        epoch_mjd = datetime2mjd_utc(epochofel)
+        ast_elems['epochofel_mjd'] = epoch_mjd
+
         print "Working on", ast_name, "at", site
         print >> ast_fh, "# %s  (H=%2.1f) at %s" % ( ast_name, ast_elems['abs_mag'], site )
         print >> ast_fh, "#UTC date/time          RA (J2000.0) Dec     PhaseA    Vmag   Alt MoonD Score  H.A. Exptime"
         
         d = obs_start
+        first_row = True
         while d <= obs_end:
             dark_start, dark_end = determine_darkness_times(site, d)
             emp = call_compute_ephem(ast_elems, dark_start, dark_end, site, obs_step, alt_limit, mag_limit)
@@ -53,10 +108,31 @@ for asteroid in bodies:
     # datetime RA Dec PhaseAngle Mag Speed  Alt MoonPhase MoonDist MoonAlt Score HA
                     exptime = determine_exptime(float(line[5]), pixel_scale, max_exptime)
                     print >> ast_fh, line_fmt % (line[0], line[1], line[2], line[3], line[4], line[6], line[8], line[10], line[11], exptime)
+                    newrow = [datetime.strptime(line[0], "%Y %m %d %H:%M"), float(line[3]), float(line[4]), float(line[5]), float(line[8]), float(line[10])]
+                    if first_row:
+                        obs_array = np.array(newrow)
+                        first_row = False
+                    else:
+                        obs_array = np.vstack([obs_array, newrow])
                 num_written += 1
             d = d + timedelta(days=1)
 
-    ast_fh.close()
-    if num_written == 0:
-        logging.warn("No lines of output produced for %s. Removing empty output file %s." % (ast_name, ast_file))
-        os.remove(ast_file)
+        ast_fh.close()
+        if num_written == 0:
+            logging.warn("No lines of output produced for %s. Removing empty output file %s." % (ast_name, ast_file))
+            os.remove(ast_file)
+        else:
+            phang_windows = determine_phaseangle_windows(obs_array, nsteps, True)
+            window = 0
+            while window < len(phang_windows):
+                window_start = phang_windows[window][0]
+                window_end = phang_windows[window][1]
+
+                subset = obs_array[np.where((obs_array[:,0] >= window_start) & (obs_array[:,0] <= window_end))]
+                mean_speed = np.mean(subset[:,3])
+                exptime = determine_exptime(mean_speed, pixel_scale, max_exptime)
+                group_id = ast_name + "_" + site + '_phasea#' + str(window+1)
+                print "Window #%2d (%s->%s), speed=%.1f, exptime=%.1f secs" % ( window, window_start, window_end, mean_speed, exptime)
+                params = determine_params(exptime, site, window_start, window_end, group_id)
+                tracking_number, resp_params = submit_block_to_scheduler(ast_elems, params)
+                window += 1
